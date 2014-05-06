069dd3588d73728d5fb677f24fe2ddc16d0cc709
mgbjjin2005 <ssid@yeah.net>
2014-05-06 17:49:48 +0800

    数据库表设计

--- sql/wifi.sql
+++ sql/wifi.sql
@@ -13,35 +13,158 @@ use panda;
 -- -----------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `panda`.`media` (
-   `m_id` bigint(20) NOT NULL AUTO_INCREMENT,
-   `m_chs_name` varchar(64) NOT NULL,/*中文名（007之皇家赌场，爱情公寓3）*/
-   `m_original_name` varchar(128) NOT NULL, /*母语名*/
-   `m_alias` varchar(64) NOT NULL,/*内部名称，(只能由字母、数字、下划线组成）*/
-   `m_series` varchar(256) DEFAULT NULL,/*系列(007，爱情公寓)*/
-   `m_episode` int NOT NULL DEFAULT 1,/*第几集(电影默认1)*/
-   `m_video_type` varchar(32) NOT DEFAULT 'moive',/*电影、电视剧、纪录片(moive,history...)*/
-   `m_show_date` date NOT NULL DEFAULT '0000-00-00',/*上映时间*/
-   `m_area_show` varchar(64) DEFAULT NULL,/*网页上显示的影片地域信息*/
-   `m_area_flag` varchar(128) DEFAULT NULL,/*用于检索的区域信息*/
-   `m_director` varchar(128) DEFAULT NULL,/*导演*/
-   `m_type` varchar(128) DEFAULT NULL,/*影片类型*/
-   `m_main_actors` varchar(256) DEFAULT NULL,/*主要演员*/
-   `m_time_length` int DEFAULT 0,/*影片时长(分钟)*/
-   `m_des` varchar(2560) DEFAULT NULL,/*剧情简介*/
+   `m_id`            bigint(20)     NOT NULL AUTO_INCREMENT,
+   `m_chs_name`      varchar(64)    NOT NULL,                       /*中文名（007之皇家赌场，爱情公寓3）*/
+   `m_original_name` varchar(128)   NOT NULL,                       /*母语名*/
+   `m_alias`         varchar(64)    NOT NULL,                       /*内部名称，(只能由字母、数字、下划线组成）*/
+   `m_series`        varchar(256)   DEFAULT NULL,                   /*系列(007，爱情公寓)*/
+   `m_episode`       int            NOT NULL DEFAULT 1,             /*第几集(电影默认1)*/
+   `m_video_type`    varchar(32)    NOT DEFAULT 'moive',            /*电影、电视剧、纪录片(moive,history...)*/
+   `m_show_date`     date           NOT NULL DEFAULT '0000-00-00',  /*上映时间*/
+   `m_area_show`     varchar(64)    DEFAULT NULL,                   /*网页上显示的影片地域信息*/
+   `m_area_flag`     varchar(128)   DEFAULT NULL,                   /*用于检索的区域信息*/
+   `m_director`      varchar(128)   DEFAULT NULL,                   /*导演*/
+   `m_type`          varchar(128)   DEFAULT NULL,                   /*影片类型*/
+   `m_main_actors`   varchar(256)   DEFAULT NULL,                   /*主要演员*/
+   `m_time_length`   int            DEFAULT 0,                      /*影片时长(分钟)*/
+   `m_des`           varchar(2560)  DEFAULT NULL,                   /*剧情简介*/
 
-   `m_total_play` int DEFAULT 0,/*累积播放次数*/
-   `m_month_play` int DEFAULT 0,/*本月播放次数*/
-   `m_day_play` int DEFAULT 0,/*昨天播放次数*/
-   `m_total_pv` int DEFAULT 0,/*影片访问pv*/
-   `m_wifi_total` DECIMAL(14,3) DEFAULT '0.0',/*影片总收入*/
-   `m_resolution` varchar(256) DEFAULT NULL,/*提供的分辨率(480P,211MB;720P,985MB)*/
-   `m_price` DECIMAL(14,3) DEFAULT '0.0',/*影片价格(wifi币)*/
-   `m_url` varchar(1024) DEFAULT NULL,/*中心的下载地址(不包括m_alias，m_alias统一构造)*/
-   `m_sata_path` varchar(1024) DEFAULT NULL,/*电影目录在sata中的绝对路径*/
-   `m_ssd_path` varchar(1024) DEFAULT NULL,/*电影目录在ssd中的路径*/
-   `m_create_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,/*记录创建时间*/
-   `m_modify_date` TIMESTAMP NULL DEFAULT NULL,/*记录修改时间*/
+   `m_total_play`    int            DEFAULT 0,                      /*累积播放次数*/
+   `m_month_play`    int            DEFAULT 0,                      /*本月播放次数*/
+   `m_day_play`      int            DEFAULT 0,                      /*昨天播放次数*/
+   `m_total_pv`      int            DEFAULT 0,                      /*影片访问pv*/
+   `m_wifi_total`    DECIMAL(14,3)  DEFAULT '0.0',                  /*影片总收入*/
+   `m_resolution`    varchar(256)   DEFAULT NULL,                   /*提供的分辨率(480P,211MB;720P,985MB)*/
+   `m_price`         DECIMAL(14,2)  DEFAULT '0.0',                  /*影片价格(wifi币)*/
+   `m_url`           varchar(1024)  DEFAULT NULL,                   /*中心的下载地址(不包括m_alias，m_alias统一构造)*/
+   `m_sata_path`     varchar(1024)  DEFAULT NULL,                   /*电影目录在sata中的绝对路径*/
+   `m_ssd_path`      varchar(1024)  DEFAULT NULL,                   /*电影目录在ssd中的路径*/
+   `m_create_date`   TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,      /*记录创建时间*/
+   `m_modify_date`   TIMESTAMP NULL DEFAULT NULL,                   /*记录修改时间*/
 
    PRIMARY KEY (`m_id`)
  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+/*用户信息表*/
+CREATE TABLE IF NOT EXISTS `panda`.`user_info` (
+   `user_name`    varchar(64)     NOT NULL,                       /*用户名*/
+   `password`     varchar(64)     NOT NULL,                       /*明文密码*/
+   `password_md5` varchar(64)     NOT NULL,                       /*密文密码*/
+   `email`        varchar(64)     NOT NULL,                       /*邮箱*/
+   `balance`      DECIMAL(14,2)   DEFAULT '0.0',                  /*账户余额*/
+   `total_cost`   DECIMAL(14,2)   DEFAULT '0.0',                  /*累积消费额*/
+   `bind_state`   varchar(16)     NOT NULL,                       /*账号绑定状态，只要绑定一个设备，状态就应该是绑定的(YES/NO)*/
+
+   `login_times`  int             DEFAULT  0,                     /*累积登录次数*/
+   `create_date`  TIMESTAMP       NULL DEFAULT CURRENT_TIMESTAMP, /*用户创建时间*/
+   `last_date`    TIMESTAMP       NULL DEFAULT NULL,              /*上次登录时间*/
+
+   PRIMARY KEY (`user_name`),
+   INDEX(`user_name`,`password`),
+   INDEX(`user_name`,`password_md5`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+/*绑定有几种情况
+一、首次使用
+    1、账号处于解绑状态
+    2、登录后系统要求绑定账号，
+    3、按照操作进行绑定
+
+二、更换手机绑定（前提是已经绑定手机A）
+    1、在手机A上登录账号
+    2、在 www.wifi.com 首页的"我的信息"下面查看自己的绑定状态，并按照提示进行解绑操作
+       解绑的过程需要通过注册时使用的邮箱做验证，只有验证通过了才能顺利解绑
+    3、解除绑定后，会自动退出账号
+    4、使用新的手机B重新登录该账号，登录后系统会提示要求绑定，按照操作进行绑定后即可完成账号绑定
+
+/*账号设备绑定表*/
+CREATE TABLE IF NOT EXISTS `panda`.`binding_info` (
+   `b_id`         bigint(20)      NOT NULL AUTO_INCREMENT,
+   `user_name`    varchar(64)     NOT NULL,                       /*用户名*/
+   `mac`          varchar(64)     NOT NULL,                       /*设备mac*/
+   `bind_state`   varchar(16)     NOT NULL,                       /*绑定状态 (YES/NO)*/
+   `valid_key`    varchar(256)    NULL,                           /*设备迁移过程时要通过邮件验证的验证key*/
+   `valid_state`  varchar(16)     DEFAULT 'done'                  /*当前验证状态("email_done":邮件已发出；"done":已经验证通过)*/
+   `create_date`  TIMESTAMP       NULL DEFAULT CURRENT_TIMESTAMP, /*创建时间*/
+
+    PRIMARY KEY (`b_id`),
+    INDEX(`user_name`, `mac`, `bind_state`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+/*电影购买记录(目前只能支持单集购买，不知道打包购买)*/
+CREATE TABLE IF NOT EXISTS `panda`.`moive_deal_info` (
+   `deal_id`      varchar(64)     NOT NULL,                       /*订单编号*/
+   `m_id`         bigint(20)      NOT NULL,                       /*视频ID*/
+   `user_name`    varchar(64)     NOT NULL,                       /*用户名*/
+   `mac`          varchar(64)     NOT NULL,                       /*购买该电影的设备*/
+   `price`        DECIMAL(14,2)   DEFAULT '0.0',                  /*购买价格*/
+   `total_cost`   DECIMAL(14,2)   DEFAULT '0.0',                  /*累积消费额*/
+   `bind_state`   varchar(16)     NOT NULL,                       /*账号绑定状态，只要绑定一个设备，状态就应该是绑定的(YES/NO)*/
+
+   `login_times`  int             DEFAULT  0,                     /*累积登录次数*/
+   `create_date`  TIMESTAMP       NULL DEFAULT CURRENT_TIMESTAMP, /*用户创建时间*/
+   `expire_date`  TIMESTAMP       NULL DEFAULT NULL,              /*资源有效期截止时间*/
+
+   INDEX(`user_name`,`mac`),
+   INDEX(`m_id`,`user_name`,`mac`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+/*流量包*/
+CREATE TABLE IF NOT EXISTS `panda`.`traffic_packet` (
+   `t_id`         bigint(20)      NOT NULL AUTO_INCREMENT,        /*套餐ID*/
+   `t_desc`       varchar(256)    NOT NULL,                       /*套餐描述*/
+   `traffic`      DECIMAL(14,2)   DEFAULT  '0.0',                 /*套餐流量*/
+   `expires`      int             DEFAULT  30,                    /*套餐有效期，默认30天*/
+   `price`        DECIMAL(14,2)   DEFAULT '0.0',                  /*价格*/
+   `create_date`  TIMESTAMP       NULL DEFAULT CURRENT_TIMESTAMP  /*套餐创建时间*/
+
+   INDEX(`user_name`,`mac`),
+   INDEX(`m_id`,`user_name`,`mac`)
+ ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+/*流量套餐表, 购买新的套餐时，如果老的套餐没有用完，会优先使用老的套餐，使用完后新的套餐流量自动启用*/
+CREATE TABLE IF NOT EXISTS `panda`.`traffic_deal_info` (
+   `t_id`         int             NOT NULL,                       /*套餐ID*/
+   `t_desc`       varchar(256)    NOT NULL,                       /*套餐描述*/
+   `expires`      int             NOT NULL,                       /*套餐有效期，默认30天*/
+   `price`        DECIMAL(14,2)   DEFAULT '0.0',                  /*购买价格*/
+   `user_name`    varchar(64)     NOT NULL,                       /*用户名*/
+   `mac`          varchar(64)     DEFAULT 'NULL',                 /*购买该套餐的设备*/
+   `state`        varchar(16)     NOT NULL,                       /*(unenable：还未启用/enable:正在使用/expire:过期)*/
+   `traffic`      DECIMAL(14,2)   DEFAULT  '0.0',                 /*套餐总流量*/
+   `traffic_used` DECIMAL(14,2)   DEFAULT  '0.0',                 /*累积使用流量*/
+   `create_date`  TIMESTAMP       NULL DEFAULT CURRENT_TIMESTAMP, /*用户创建时间*/
+   `expire_date`  TIMESTAMP       NULL DEFAULT NULL,              /*资源有效期截止时间*/
+
+   INDEX(`user_name`,`state`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+/*电影购物车*/
+CREATE TABLE IF NOT EXISTS `panda`.`cart` (
+   `user_name`    varchar(64)     NOT NULL,                       /*用户名*/
+   `m_id`         bigint(20)      NOT NULL,                       /*视频ID*/
+   `m_desc`       varchar(128)    NOT NULL,                       /*视频信息描述（蜘蛛侠系列之超凡蜘蛛侠、爱情公寓四第18集）*/
+   `price`        DECIMAL(14,2)   DEFAULT '0.0',                  /*购买价格*/
+   `expire`       int             DEFAULT  0,                     /*资源购买后的有效期，单位为天，对应global_conf.moive_expire*/
+   `expire_date`  TIMESTAMP       NULL DEFAULT NULL,              /*该信息在购物车的有效期*/
+
+   INDEX(`user_name`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+/*全局配置项*/
+CREATE TABLE IF NOT EXISTS `panda`.`global_conf` (
+   `station_name` varchar(64)     NOT NULL,                       /*节点名称*/
+   `moive_expire` int             DEFAULT   3,                    /*购买后视频的有效期*/
+   `m_charge_off` int             DEFAULT   100,                  /*电影是否打折*/
+   `t_charge_off` int             DEFAULT   100,                  /*流量是否打折*/
+
+   INDEX(`station_name`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
